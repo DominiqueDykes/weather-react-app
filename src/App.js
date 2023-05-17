3e9678503bf3ef986623cdb4b6c7790a63eb8591
DominiqueDykes <nique.dykes@gmail.com>
2023-05-17 12:26:03 -0400

    moved files from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello World!!!
+        </h1>
+        <Weather />
         <a
           className="App-link"
           href="https://reactjs.org"
